771017b2bffe06af0b30521b46ab00b7e4c928e1
zhangsheng <2402536045@qq.com>
2022-08-27 08:08:53 +0800

    Vue3 基础刷题

---
+++ upload-vue/src/views/vuejsChallenges/243-prevent-event-propagation.vue
@@ -0,0 +1,22 @@
+<!-- 阻止事件冒泡 -->
+<script setup lang="ts">
+
+const click1 = () => {
+  console.log('click1')
+}
+
+const click2 = (e: Event) => {
+  e.stopPropagation()
+  console.log('click2')
+}
+
+</script>
+
+<template>
+  <div @click="click1()">
+  <!-- div @click.stop="click2()" -->
+   <div @click="click2($event)">
+     click me
+   </div>
+  </div>
+</template>
